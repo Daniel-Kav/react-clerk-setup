f3829ef67c12e80713923bb1d3101ec16dad1a19
daniel-kav <dankavatha01@gmail.com>
2025-01-25 10:38:50 +0300

    Wrap App component with ClerkProvider and add Home page component

---
+++ Api/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is a public page accessible to everyone.</p>
+      </div>
+    );
+  }
