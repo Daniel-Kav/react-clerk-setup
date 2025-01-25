093a9aa09a5d541851259f20cf84823e3b5db2d2
daniel-kav <dankavatha01@gmail.com>
2025-01-25 11:07:08 +0300

    Remove BooksTable schema and related migration files; add UsersTable schema for user management

--- client/src/drizzle/schema.ts
+++ client/src/drizzle/schema.ts
@@ -1,13 +1,13 @@
-import { pgTable, serial, varchar, integer } from 'drizzle-orm/pg-core';
+import { pgTable, varchar, timestamp } from 'drizzle-orm/pg-core';
 
-// Define the BooksTable schema
-export const BooksTable = pgTable('books', {
-  id: serial('id').primaryKey(),
-  title: varchar('title', { length: 255 }),
-  author: varchar('author', { length: 100 }),
-  year: integer('year'),  // Nullable if year is optional in the database
+export const users = pgTable('users', {
+  id: varchar('id').primaryKey(),
+  clerkId: varchar('clerk_id').notNull(),
+  firstName: varchar('first_name'),
+  lastName: varchar('last_name'),
+  email: varchar('email').notNull(),
+  createdAt: timestamp('created_at').defaultNow(),
 });
 
-// Types
-export type TIBook = typeof BooksTable.$inferInsert;
-export type TSBook = typeof BooksTable.$inferSelect;
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
